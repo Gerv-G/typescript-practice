4667fe819a011a72acd6c01e0cfdf820e4871635
Gerv-G <25771885+Gerv-G@users.noreply.github.com>
2022-05-06 16:23:25 +0800

    Modify test to verify sum for multiple number input

--- tests/stringCalculator.test.ts
+++ tests/stringCalculator.test.ts
@@ -6,15 +6,20 @@ test('empty input should return 0', () => {
     expect(result).toBe(0);
 });
 
+test('null input should return 0', () => {
+    let input = null;
+    let result = stringCalculator.add(input);
+    expect(result).toBe(0);
+})
+
 test('single input should return itself', () => {
     let input = "5";
     let result = stringCalculator.add(input);
     expect(result).toBe(5);
 });
 
-test('two-number input should return the sum', () => {
-    let input = "1,2";
+test('comma-separated numbers input should return the sum', () => {
+    let input = "1,2,3,4";
     let result = stringCalculator.add(input);
-    expect(result).toBe(3);
+    expect(result).toBe(10);
 });
-
